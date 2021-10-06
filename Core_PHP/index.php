fb56ee7bfca915fafb328ffdf829a4a6a3ba987e
shreetuition <info@shreetuitioncenter.com>
2021-10-06 16:40:54 +0530

    Done

---
+++ Core_PHP/index.php
@@ -0,0 +1,177 @@
+<?php
+	
+	// echo "Hello This is Kumar";
+	// echo 'string';
+
+	//echo "<h1>Hello</h1>";
+
+	// PHP Variables
+
+	// A variable can have a short name (like x and y) or a more descriptive name (age, carname, total_volume).
+
+	// Rules for PHP variables:
+
+	// A variable starts with the $ sign, followed by the name of the variable
+	// A variable name must start with a letter or the underscore character
+	// A variable name cannot start with a number
+	// A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+	// Variable names are case-sensitive ($age and $AGE are two different variables)
+
+	// echo $total_volume = 5;
+
+	// echo $total_volume;
+		
+		// echo $toasdsa551654tal = 5;
+
+	// $color = "red";
+	// $Color = "green";
+
+	// echo $Color;
+	// echo "<br>";
+	// echo $color;
+
+	
+	/*+
+	-
+	*
+	/
+	%*/
+
+	// $firstName = "Sharvan";
+
+	// $lastName = "Kumar";
+	// $space = " ";
+	// echo $firstName." ".$lastName;
+	// echo $firstName;
+	// echo "<br>";
+	// echo $lastName;
+
+	// echo "<br>";
+
+	// $a = (int) "5";
+	// $b = (int) "3";
+
+	// var_dump($a);
+
+	// echo $a+$b;
+	// echo "<br>";
+	// echo $a-$b;
+	// echo "<br>";
+	// echo $a*$b;
+	// echo "<br>";
+	// echo $a/$b;
+	// echo "<br>";
+	// echo $a%$b;
+	// echo "<br>";
+
+	// True = 1
+	// false = 0
+	// >
+	// =
+	// >=
+	// ==
+	// ===
+	// <=
+	// <
+	// ! => Not 
+
+	// !=
+
+	// $x = 8;
+	// $y = 6;
+	// $z = 10;
+
+	// echo $x==$y;
+	// echo $x>$y;
+	// echo $x<$y;
+	// echo $x>=$y;
+	// echo $x<$y;
+	// echo $x<=$y;
+
+	//echo $x===$y;
+
+
+	//echo $x!=$y;
+
+	// echo $x==8;
+	// die;
+
+	//echo $x==8 && $y==6;
+
+	//($x==8 OR $y==6 OR $z==5);
+
+	// if ($x==9 OR $y==10 OR $z==10) {
+	// 	echo "True";
+	// } else {
+	// 	echo "False";
+	// }
+
+	// Logic operator
+
+	// And
+	// or
+
+	// $x++
+	// $x = $x + 1
+
+	// preincrement ++$x 
+	// postincrement $x++
+
+	// echo $x = 10; // 10
+
+	// echo "<br>";
+
+	// echo $x++; // 11 
+
+	// echo "<br>";
+
+	// echo $x; // 11
+
+	// echo $x = 10; // 10
+
+	// echo "<br>";
+
+	// echo ++$x; // 11 
+
+	// echo "<br>";
+
+	// echo $x; // 11
+
+	// calculate area of circle
+
+	// = pi*r*r
+
+	// PA = 100000 => Axis Bank 
+	// Time = 2 Yrs 
+	// ROI = 15% 
+
+	// TA = 
+	// TP = 
+
+	$p = 100000;
+	$r = 15;
+	$t = 2;
+
+	$si = ($p * $r * $t)/100;
+
+	echo $toatalAmount = $p + $si;
+	echo "<br>";
+	echo $si;
+
+	// $pi = 3.14;
+	// $radius = 5;
+
+	// $area = ($pi * $radius * $radius); 	
+
+	// echo $area;
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<h1>This is Sharvan <?php echo "Kumar"; ?></h1>
+</body>
+</html>
